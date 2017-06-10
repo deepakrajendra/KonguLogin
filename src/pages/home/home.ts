a17df0cafd2f9a52face281184e6c0ee2ad5f036
DeepakRajendra <deepakrbabudev@gmail.com>
2017-06-10 23:46:19 +0530

    Initial version

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,13 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private inApp: InAppBrowser) {
 
   }
 
+  open() {
+
+    const browser = this.inApp.create('http://122.165.102.21:8081/', '_self', 'location=no');
+
+  }
 }
